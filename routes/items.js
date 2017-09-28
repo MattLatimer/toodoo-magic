a753696c3f2e3d6d5b85b36bbc23a419e7d6fb87
Andy Leung <andy_ch_009@hotmail.com>
2017-09-28 22:02:02 +0000

    adding getKeyword function

--- routes/items.js
+++ routes/items.js
@@ -6,6 +6,34 @@ const router  = express.Router();
 // localhost:8080/items/
 
 module.exports = (knex) => {
+  // Helper functions
+  // getKeyword: returns one of the four keywords OR the entire string which was passed in
+  function getKeyword(str) {
+    for (const i = 0; i < str.length; i++) {
+      if (str[i] === ' ') {
+        const end = i;
+        break;
+      }
+    }
+    const verb = str.splice(0, end);
+    if (verb === 'eat' || verb === 'watch' || verb === 'read' || verb === 'buy') {
+      return verb;
+    }
+    else {
+      for (const i = str.length - 1; i > 0; i--) {
+        if (str[i] === ' ') {
+          const start = i;
+          break;
+        }
+      }
+      const verb = str.splice(start, str.length);
+      if (verb === 'eat' || verb === 'watch' || verb === 'read' || verb === 'buy') {
+        return verb;
+      }
+    }
+    return str;
+  }
+
   router.get("/", (req, res) => {
     // knex
       // retrieve from items table
