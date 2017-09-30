085504a35bc994a51f212901ff2587fe5673974a
Parmvir <parmvirsthind@gmail.com>
2017-09-30 20:07:02 +0000

    Corrected items seeds

--- db/seeds/3_items.js
+++ db/seeds/3_items.js
@@ -6,10 +6,10 @@ exports.seed = function(knex, Promise) {
         knex('items').insert({content: 'watch Game of Thrones', users_id: 2, categories_id: 1}),
         knex('items').insert({content: 'read Slaughterhouse Five', users_id: 1, categories_id: 2}),
         knex('items').insert({content: 'read Sirens of Titan', users_id: 2, categories_id: 2}),
-        knex('items').insert({content: 'buy Rolex', users_id: 1, categories_id: 3}),
-        knex('items').insert({content: 'buy Fidget spinner', users_id: 2, categories_id: 3}),
-        knex('items').insert({content: 'eat potatoes', users_id: 1, categories_id: 4}),
-        knex('items').insert({content: 'eat at Oakwood Bistro', users_id: 1, categories_id: 4})
+        knex('items').insert({content: 'eat potatoes', users_id: 1, categories_id: 3}),
+        knex('items').insert({content: 'eat at Oakwood Bistro', users_id: 1, categories_id: 3}),
+        knex('items').insert({content: 'buy Rolex', users_id: 1, categories_id: 4}),
+        knex('items').insert({content: 'buy Fidget spinner', users_id: 2, categories_id: 4}),
       ]);
     });
 };
