d022ef60ee7d837b4e0423b15675b6b6fd95f801
Matt Latimer <matt.latimer@gmail.com>
2017-09-30 18:07:29 -0700

    login page and routes

--- routes/general.js
+++ routes/general.js
@@ -16,18 +16,10 @@ module.exports = (knex) => {
       res.render('register');
     }
     res.render("index");
-  })
-
-  router.get("/login", (req, res) => {
-    req.session.user_id = 1;
-    res.locals.user_id = 1;
-    // req.session = null;
-    console.log("redirected!");
-    res.redirect("/");
-  })
-
-
-
+  });
+  
+  
+  
   function findByEmail(email) {
     return new Promise((resolve, reject) => {
       knex('users')
@@ -39,9 +31,9 @@ module.exports = (knex) => {
           return (user) ? resolve(user) : reject(email);
         })
         .catch((error) => reject(error));
-    })
+    });
   }
-
+    
   function checkEmailUniqueness(email) {
     return new Promise((resolve, reject) => {
       findByEmail(email)
@@ -55,10 +47,10 @@ module.exports = (knex) => {
             return resolve(email);
           }
         })
-        .catch((email) => resolve(email))
-    })
+        .catch((email) => resolve(email));
+    });
   }
-
+    
   function add(email, password, name) {
     return (
       checkEmailUniqueness(email) // First check if email already exists
@@ -71,19 +63,41 @@ module.exports = (knex) => {
           return knex('users').insert({
             email: email,
             pw_hash: passwordDigest,
-            full_name: name,
+            full_name: name
           })
-        .returning('id');
+            .returning('id');
         })
-    //     // .catch((error) => { 
+        //     // .catch((error) => { 
     //     //   console.log("function add error:", error)
     //     //   return reject(error);
     //   })
-    )
+    );
   }
-
-
-
+      
+  router.get("/login", (req, res) => {
+    res.locals.error = '';
+    res.render("login");
+  });
+      
+  router.post("/login", (req, res) => {
+    findByEmail(req.body.email)
+      .then((result) => {
+        bcrypt.compare(req.body.password, result.pw_hash, (err, pwMatch) => {
+          if (pwMatch) {
+            req.session.user_id = result.id;
+            res.redirect('/');
+          } else {
+            res.locals.error = "Bad credentials.";
+            res.render("login");
+          }
+        });
+      })
+      .catch((error) => {
+        res.locals.error = "Bad credentials.";
+        res.render("login");
+      });
+  });
+      
   router.post("/register", (req, res) => {
     let email = req.body.email;
     let pw = req.body.password;
@@ -91,28 +105,27 @@ module.exports = (knex) => {
     add(email, pw, name).then((result) => {
       console.log(result);
       if (result != undefined){
-      let id = result[0];
-      req.session.user_id = id;
-      console.log("You have created your account and been logged in.");
-      res.redirect('/');
-      }
-      else {
-      return(error);
-      res.redirect('/register');
+        let id = result[0];
+        req.session.user_id = id;
+        console.log("You have created your account and been logged in.");
+        res.redirect('/');
+      } else {
+        return(error);
+        res.redirect('/register');
       }
     })
-    .catch((error) => { 
-    console.log(error, "Unsuccessful Login.");
-    res.status(400).send(error);
-    })
-  })
+      .catch((error) => {
+        console.log(error, "Unsuccessful Login.");
+        res.status(400).send(error);
+      });
+  });
 
 
 
 
   router.get("/register", (req, res) => {
     res.render("register");
-  })
+  });
 
   router.get("/logout", (req, res) => {
     req.session.user_id = null;
@@ -120,7 +133,7 @@ module.exports = (knex) => {
     // req.session = null;
     res.redirect("/register");
     console.log("redirected!");
-  })
+  });
 
   return router;
-}
+};
